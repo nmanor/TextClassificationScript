fcdc06fff20ae54e167d271a5bc36025e1e7dae2
Yair Yigal <yigalyairn@gmail.com>
2018-12-18 18:08:46 +0000

    Update system_config.py

--- system_config.py
+++ system_config.py
@@ -1,8 +1,7 @@
 EMAIL_USERNAME = 'pythonnotificationpushup@gmail.com'
 EMAIL_PASSWORD = 'kerneristhebest'
 
-YAIR = 164199488
-DANMI = 174780975
+USERS = [] # Add your telegram chat id here
 BOT_TOKEN = '491291630:AAH8QvUTqd3yLYerSF01geomonPgcgrMa2M'
 
 DICTIONARY_PATH = ".\\words\\words.json"
